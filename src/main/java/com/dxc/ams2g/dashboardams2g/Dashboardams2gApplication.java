25a3e5cefd626589d789dd22cbbc797cf72fb674
rbruni <anonymousa555@gmail.com>
2022-02-28 10:15:36 +0100

    Initial commit

---
+++ src/main/java/com/dxc/ams2g/dashboardams2g/Dashboardams2gApplication.java
@@ -0,0 +1,13 @@
+package com.dxc.ams2g.dashboardams2g;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dashboardams2gApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Dashboardams2gApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dxc/ams2g/dashboardams2g/Dashboardams2gApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dxc.ams2g.dashboardams2g;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dashboardams2gApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
