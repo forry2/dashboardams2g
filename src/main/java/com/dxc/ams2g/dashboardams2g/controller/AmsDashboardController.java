03ac7a61624d61fe606c72c63a8916037e4ffb13
rbruni <anonymousa555@gmail.com>
2022-03-02 01:54:44 +0100

    aggiunti servizi di conteggio abbinamenti per massima data di caricamento dati grezzi

--- src/main/java/com/dxc/ams2g/dashboardams2g/controller/AmsDashboardController.java
+++ src/main/java/com/dxc/ams2g/dashboardams2g/controller/AmsDashboardController.java
@@ -2,14 +2,14 @@ package com.dxc.ams2g.dashboardams2g.controller;
 
 import com.dxc.ams2g.dashboardams2g.service.AmsDashboardService;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import org.bson.Document;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
+import java.util.List;
 
 @RestController
 @RequestMapping
@@ -17,10 +17,45 @@ public class AmsDashboardController {
     @Autowired
     private AmsDashboardService service;
 
-    @GetMapping(value = "switch/matching")
-    public ResponseEntity<String> findMatching(
+    @GetMapping(value = "switch/matching/csv")
+    public ResponseEntity<String> findMatchingSwitch(
             @RequestParam(name = "dateFrom", required = false) Date dateFrom
     ) throws JsonProcessingException {
         return ResponseEntity.ok(service.findMatchingSwitchCsv(dateFrom));
+    }
+
+    @GetMapping(value = "volture/matching/csv")
+    public ResponseEntity<String> findMatchingVolture(
+            @RequestParam(name = "dateFrom", required = false) Date dateFrom
+    ) throws JsonProcessingException {
+        return ResponseEntity.ok(service.findMatchingVoltureCsv(dateFrom));
+    }
+
+    @GetMapping(value = "switch/matching/maxUploadDate/{maxUploadDate}")
+    public ResponseEntity<List<Document>> findMatchingSwitchMaxUploadDate(
+            @PathVariable(value = "maxUploadDate") @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'") Date maxUploadDate
+    ) {
+        return ResponseEntity.ok(service.findMatchingSwitchMaxUploadDate(maxUploadDate == null ? new Date(): maxUploadDate));
+    }
+
+    @GetMapping(value = "switch/matching/maxUploadDate/{maxUploadDate}/count")
+    public ResponseEntity<Integer> findMatchingSwitchMaxUploadDateCount(
+            @PathVariable(value = "maxUploadDate") @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'") Date maxUploadDate
+    ){
+        return ResponseEntity.ok(service.findMatchingSwitchMaxUploadDate(maxUploadDate == null ? new Date(): maxUploadDate).size());
+    }
+
+    @GetMapping(value = "volture/matching/maxUploadDate/{maxUploadDate}")
+    public ResponseEntity<List<Document>> findMatchingVoltureMaxUploadDate(
+            @PathVariable(value = "maxUploadDate") @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'") Date maxUploadDate
+    ) {
+        return ResponseEntity.ok(service.findMatchingVoltureMaxUploadDate(maxUploadDate == null ? new Date(): maxUploadDate));
+    }
+
+    @GetMapping(value = "volture/matching/maxUploadDate/{maxUploadDate}/count")
+    public ResponseEntity<Integer> findMatchingVoltureMaxUploadDateCount(
+            @PathVariable(value = "maxUploadDate") @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'") Date maxUploadDate
+    ){
+        return ResponseEntity.ok(service.findMatchingVoltureMaxUploadDate(maxUploadDate == null ? new Date(): maxUploadDate).size());
     }
 }
--- src/main/java/com/dxc/ams2g/dashboardams2g/service/AmsDashboardService.java
+++ src/main/java/com/dxc/ams2g/dashboardams2g/service/AmsDashboardService.java
@@ -13,6 +13,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.aggregation.AggregationOperation;
+import org.springframework.data.mongodb.core.aggregation.AggregationOperationContext;
+import org.springframework.data.mongodb.core.aggregation.AggregationOptions;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.stereotype.Service;
 
@@ -24,34 +26,112 @@ import static org.springframework.data.mongodb.core.aggregation.Aggregation.*;
 
 @Service
 public class AmsDashboardService {
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
     @Autowired
     private MongoTemplate mongoTemplate;
-    private final Logger log = LoggerFactory.getLogger(this.getClass());
 
     public String findMatchingSwitchCsv(Date dateFrom) throws JsonProcessingException {
 
-        ArrayList<AggregationOperation> aggrList =
-                new ArrayList<>();
+        ArrayList<AggregationOperation> aggrList = new ArrayList<>();
         Criteria criteria = new Criteria();
         criteria = criteria.and("lettura").ne(null).and("dettaglioPubblicazione").ne(null);
         aggrList.add(match(criteria));
         aggrList.add(project("DSWITC", "IDN_UTEN_ERN").andExclude("_id"));
         aggrList.add(sort(Sort.Direction.DESC, "DSWITC", "IDN_UTEN_ERN"));
-        List<Document> retList = mongoTemplate
-                .aggregate(newAggregation(aggrList), "dashboardAms2gSwitch", Document.class)
-                .getMappedResults();
+        List<Document> retList = mongoTemplate.aggregate(newAggregation(aggrList), "dashboardAms2gSwitch", Document.class).getMappedResults();
         log.debug("Found {} results", retList.size());
-        JsonNode jsonTree = new ObjectMapper().readTree(
-                new ObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(retList)
-        );
+        JsonNode jsonTree = new ObjectMapper().readTree(new ObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(retList));
         Builder csvSchemaBuilder = CsvSchema.builder();
         JsonNode firstObject = jsonTree.elements().next();
         firstObject.fieldNames().forEachRemaining(csvSchemaBuilder::addColumn);
         CsvSchema csvSchema = csvSchemaBuilder.build().withHeader();
         CsvMapper csvMapper = new CsvMapper();
-        return
-                csvMapper.writerFor(JsonNode.class)
-                        .with(csvSchema)
-                        .writeValueAsString(jsonTree);
+        return csvMapper.writerFor(JsonNode.class).with(csvSchema).writeValueAsString(jsonTree);
+    }
+
+    public String findMatchingVoltureCsv(Date dateFrom) throws JsonProcessingException {
+        ArrayList<AggregationOperation> aggrList = new ArrayList<>();
+        Criteria criteria = new Criteria();
+        criteria = criteria.and("lettura").ne(null).and("dettaglioPubblicazione").ne(null);
+        aggrList.add(match(criteria));
+        aggrList.add(project("DVOLTU", "IDN_UTEN_ERN").andExclude("_id"));
+        aggrList.add(sort(Sort.Direction.DESC, "DVOLTU", "IDN_UTEN_ERN"));
+        List<Document> retList = mongoTemplate.aggregate(newAggregation(aggrList), "dashboardAms2gVolture", Document.class).getMappedResults();
+        log.debug("Found {} results", retList.size());
+        JsonNode jsonTree = new ObjectMapper().readTree(new ObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(retList));
+        Builder csvSchemaBuilder = CsvSchema.builder();
+        JsonNode firstObject = jsonTree.elements().next();
+        firstObject.fieldNames().forEachRemaining(csvSchemaBuilder::addColumn);
+        CsvSchema csvSchema = csvSchemaBuilder.build().withHeader();
+        CsvMapper csvMapper = new CsvMapper();
+        return csvMapper.writerFor(JsonNode.class).with(csvSchema).writeValueAsString(jsonTree);
+    }
+
+    public List<Document> findMatchingSwitchMaxUploadDate(Date maxUploadDate) {
+        ArrayList<AggregationOperation> aggrList = new ArrayList<>();
+        aggrList.add(match(Criteria.where("dataUploadDateTime").lte(maxUploadDate)));
+        aggrList.add(group("IDN_UTEN_ERN", "DSWITC", "dataSource").count().as("count"));
+        AggregationOperation customGroupAggrOperation = context -> {
+            Document idDoc = new Document();
+            idDoc.put("IDN_UTEN_ERN", "$_id.IDN_UTEN_ERN");
+            idDoc.put("DSWITC", "$_id.DSWITC");
+            Document sourcesDoc = new Document("$push", "$_id.dataSource");
+            Document groupDoc = new Document();
+            groupDoc.put("_id", idDoc);
+            groupDoc.put("sources", sourcesDoc);
+            return new Document("$group", groupDoc);
+        };
+        aggrList.add(customGroupAggrOperation);
+        AggregationOperation customAddFieldsOperation = aoc -> {
+            Document sizeSourcesDoc = new Document("$size", "$sources");
+            Document sourceCountDoc = new Document("sourcesCount", sizeSourcesDoc);
+            return new Document("$addFields", sourceCountDoc);
+        };
+        aggrList.add(customAddFieldsOperation);
+        AggregationOperation customProjectOperation = aoc -> {
+            Document projDoc = new Document();
+            projDoc.put("_id", 0);
+            projDoc.put("IDN_UTEN_ERN", "$_id.IDN_UTEN_ERN");
+            projDoc.put("DSWITC", "$_id.DSWITC");
+            projDoc.put("sourcesCount", 1);
+            return new Document("$project", projDoc);
+        };
+        aggrList.add(customProjectOperation);
+        aggrList.add(match(Criteria.where("sourcesCount").gte(2)));
+        return mongoTemplate.aggregate(newAggregation(aggrList).withOptions(AggregationOptions.builder().allowDiskUse(true).build()), "dashboardAms2gSwitchRaw", Document.class).getMappedResults();
+    }
+
+    public List<Document> findMatchingVoltureMaxUploadDate(Date maxUploadDate) {
+        ArrayList<AggregationOperation> aggrList = new ArrayList<>();
+        aggrList.add(match(Criteria.where("dataUploadDateTime").lte(maxUploadDate)));
+        aggrList.add(group("IDN_UTEN_ERN", "DVOLTU", "dataSource").count().as("count"));
+        AggregationOperation customGroupAggrOperation = context -> {
+            Document idDoc = new Document();
+            idDoc.put("IDN_UTEN_ERN", "$_id.IDN_UTEN_ERN");
+            idDoc.put("DVOLTU", "$_id.DVOLTU");
+            Document sourcesDoc = new Document("$push", "$_id.dataSource");
+            Document groupDoc = new Document();
+            groupDoc.put("_id", idDoc);
+            groupDoc.put("sources", sourcesDoc);
+            return new Document("$group", groupDoc);
+        };
+        aggrList.add(customGroupAggrOperation);
+        AggregationOperation customAddFieldsOperation = aoc -> {
+            Document sizeSourcesDoc = new Document("$size", "$sources");
+            Document sourceCountDoc = new Document("sourcesCount", sizeSourcesDoc);
+            return new Document("$addFields", sourceCountDoc);
+        };
+        aggrList.add(customAddFieldsOperation);
+        AggregationOperation customProjectOperation = aoc -> {
+            Document projDoc = new Document();
+            projDoc.put("_id", 0);
+            projDoc.put("IDN_UTEN_ERN", "$_id.IDN_UTEN_ERN");
+            projDoc.put("DVOLTU", "$_id.DVOLTU");
+            projDoc.put("sourcesCount", 1);
+            return new Document("$project", projDoc);
+        };
+        aggrList.add(customProjectOperation);
+        aggrList.add(match(Criteria.where("sourcesCount").gte(2)));
+        return mongoTemplate.aggregate(newAggregation(aggrList).withOptions(AggregationOptions.builder().allowDiskUse(true).build()), "dashboardAms2gVoltureRaw", Document.class).getMappedResults();
     }
 }
